23de5644f8157b43d04dcbe89d056d3d7e69e336
guerrerocing <guerrerocing@outlook.com>
2023-09-11 19:22:00 -0600

    feat: server routes added to express server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import userRoutes from "./routes/userRoutes";
+import transactionRoutes from "./routes/transactionRoutes";
+import { errorHandler } from "./middleware/errorMiddleware";
 
 const app = express();
 
@@ -9,5 +12,12 @@ app.use(cors());
 
 // Parse JSON request bodies
 app.use(bodyParser.json());
+
+// Define API routes
+app.use("/api/users", userRoutes);
+app.use("/api/transactions", transactionRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
 
 export default app;
